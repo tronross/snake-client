3af73db2ed4e302a05c0568435d308215415a26e
tronross <john_ross@analogforever.ca>
2022-10-19 17:19:07 -0400

    Added key mappings for arrow keys as alternate control surface; comments

--- input.js
+++ input.js
@@ -16,19 +16,21 @@ const handleUserInput = function(uInput) {
   // \u0003 maps to ctrl+c input to terminate game
   if (uInput === '\u0003') {
     process.exit();
-  } else if (uInput === '\u0077') {
+    // direction key mappings for WASD and (or) arrow keys (as an alternate control surface)
+  } else if (uInput === '\u0077' || uInput === "\033[A") { 
     connection.write("Move: up");
-  } else if (uInput === '\u0061') {
+  } else if (uInput === '\u0061' || uInput === "\033[D") {
     connection.write("Move: left");
-  } else if (uInput === '\u0073') {
+  } else if (uInput === '\u0073' || uInput === "\033[B") {
     connection.write("Move: down");
-  } else if (uInput === '\u0064') {
+  } else if (uInput === '\u0064' || uInput === "\033[C") {
     connection.write("Move: right");
-  } else if (uInput === '\u006C') {
+    // key mappings to make the snake talk
+  } else if (uInput === '\u006C') {  // l maps to:
     connection.write("Say: Meowz");
-  } else if (uInput === '\u0070') {
+  } else if (uInput === '\u0070') {  // p maps to:
     connection.write("Say: Yaasssssss");
-  } else if (uInput === '\u0071') {
+  } else if (uInput === '\u0071') {  // q maps to:
     connection.write("Say: Abra 4ever");
   }
   
