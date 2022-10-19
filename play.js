8e0a4b655797b54d1ba2324ee9594b714740f535
tronross <john_ross@analogforever.ca>
2022-10-18 21:11:47 -0400

    Defined setupInput() to handle stdin, and handleUserInput() with ctrl+c mapped to process.exit().

--- play.js
+++ play.js
@@ -1,1 +1,20 @@
 const { connect } = require('./client.js')
+
+// setup interface to handle user input from stdin
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.on("data", handleUserInput);
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function() {
+  // \u0003 maps to ctrl+c input
+if (key === '\u0003') {
+  process.exit();
+}
+
+};
