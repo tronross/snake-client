429d197890ab3c924e74394808d9ca0e5982e6c2
tronross <john_ross@analogforever.ca>
2022-10-18 20:25:59 -0400

    Connection messaging, write name to server

--- client.js
+++ client.js
@@ -1,10 +1,20 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
+  console.log("Connecting ..."); // notify user function has been called
+
   const conn = net.createConnection({
     host: 'localhost',
     port: 50541,
+  });
+
+  conn.on ("connect", () => {
+    console.log("Connected to game server");
+  });
+
+  conn.on("connect", () => {
+    conn.write("Name: 555");
   });
 
   // interpret incoming data as text
@@ -13,11 +23,10 @@ const connect = function () {
   // handle incoming data and log it to the console
   conn.on("data", (data) => {
     console.log("Msg from client: ", data);
-  })
+  });
   return conn;
 };
 
-console.log("Connecting ...");
 connect();
 
-modules.export = connect;
+module.exports = { connect, };
