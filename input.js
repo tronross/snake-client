f96a091b5881e4b1d85f76a5bdb9b4387a7b3b60
tronross <john_ross@analogforever.ca>
2022-10-19 14:59:03 -0400

    Added key mappings for WASD and console.log test code to check success of mapping.

--- input.js
+++ input.js
@@ -1,5 +1,8 @@
+let connection;
+
 // setupInput: setup interface to handle user input from stdin
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -13,7 +16,16 @@ const handleUserInput = function(uInput) {
   // \u0003 maps to ctrl+c input to terminate game
   if (uInput === '\u0003') {
     process.exit();
-  }
+  } else if (uInput === '\u0077') {
+    console.log("Sweeet sweet w yass");
+  } else if (uInput === '\u0061') {
+    console.log("aaaaaaaaaaaaaaaaaaaaa");
+  } else if (uInput === '\u0073') {
+    console.log("snekssssssssss");
+  } else if (uInput === '\u0064') {
+    console.log("dd d d d d d d dd");
+  };
+  
 
 };
 
